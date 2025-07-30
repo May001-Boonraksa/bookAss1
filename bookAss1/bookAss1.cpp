cfdfae96bc629c81ebd42fe38c4130a01e9a98ab
15358 <boonraksa76@gmail.com>
2025-07-30 19:04:40 +1000

    Add more books

--- bookAss1/bookAss1.cpp
+++ bookAss1/bookAss1.cpp
@@ -7,12 +7,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Testing";
+
     Book Book1;
-    Book1.setBookDetails("Saturn", "THUYO", "May", true);
+    Book1.setBookDetails("Don Quixote", "AAAAA", "Miguel de Cervantes", true);
     Book Book2;
-    Book2.setBookDetails("Moon", "JUMNB", "Kim", true);
+    Book2.setBookDetails("Jane Eyre", "BBBBB", "Charlotte Brontë", true);
     Book Book3;
-    Book3.setBookDetails("Saturn", "THUYO", "May", true);
+    Book3.setBookDetails("The Picture of Dorian Gray", "CCCCC", "Oscar Wilde", true);
+    Book Book4;
+    Book4.setBookDetails("Pride and Prejudice", "DDDDD", "Jane Austen", true);
+    Book Book5;
+    Book5.setBookDetails("Wuthering Heights", "EEEEE", "Emily Brontë", true);
+
+    Book array[5];
+    array[0] = Book1;
+    array[1] = Book2;
+    array[2] = Book3;
+    array[3] = Book4;
+    array[4] = Book5;
+
+    string isbnToBorrow = "";
+    while (isbnToBorrow != "0") {
+        cout << "Input the book's ISBN you want to borrow: ";
+        cin >> isbnToBorrow;
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (array[i].isbn == isbnToBorrow) {
+                if (array[i].borrowBook()) {
+                    cout << "Don't forget to return the book: " << array[i].title << endl;
+                    break;
+                }
+                else {
+                    cout << "The book is unavailable";
+                }
+            }
+        }
+
+    }
+
 }
+
 
